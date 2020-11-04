acec2ae4b8b9b06586d5b4e8f03fad7c70ec3fba
Luis Bascur <luis.bascur89@gmail.com>
2020-11-04 16:20:00 -0300

    update

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 
 //Model
 type Producto struct {
-	ID     int64
-	Nombre string
-	Desc   string
+	ID     int64  `json:"id"`
+	Nombre string `json:"nombre"`
+	Desc   string `json:"desc"`
 }
 
 // Slice = Array
@@ -37,5 +37,24 @@ func main() {
 	r.HandleFunc("/api/productos", getProductos).Methods("GET")
 
 	// Servir app
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", setHeaders(r)))
+}
+
+func setHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Content-Type", "application/json, text/plain")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Content-Type, Apikey, "+
+				"Access-Control-Allow-Origin, Access-Control-Allow-Headers, Access-Control-Allow-Methods,  Origin, x-ibm-client-id, X-Authorization-Type, X-System-Type")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+			return
+		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Strict-Transport-Security", "max-age=86400; includeSubDomains")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		next.ServeHTTP(w, r)
+	})
 }
